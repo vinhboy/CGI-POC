1e7fecc01b5e0536b32697a2d8ce313ba79356d2
venkat.mareedu <venkat.mareedu@cgi.com>
2017-02-15 14:23:48 -0600

    Added TimeStamp for taking screenshots

--- selenium/com.ChildWelfareAutomationSuite.POC/src/test/java/com/ChildWelfareAutomationSuite/Utilities/GenericFunctions.java
+++ selenium/com.ChildWelfareAutomationSuite.POC/src/test/java/com/ChildWelfareAutomationSuite/Utilities/GenericFunctions.java
@@ -17,10 +17,11 @@ public class GenericFunctions {
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		File src = ts.getScreenshotAs(OutputType.FILE);
 		System.out.println("Source Path" + src);
-		String destination = "../Screenshots/" + Screenshotname + ".PNG";
+		String destination = "../Screenshots/" + Screenshotname + System.currentTimeMillis()+".PNG";
 
 		try {
 			FileUtils.copyFile(src, new File("./Screenshots/" + Screenshotname + ".PNG"));
+			System.out.println("Screenshot copied to destination Screenshots folder Sucessfully"+destination);
 		} catch (IOException e) {
 			System.out.println("Failed to take Screenshot" + e.getMessage());
 
