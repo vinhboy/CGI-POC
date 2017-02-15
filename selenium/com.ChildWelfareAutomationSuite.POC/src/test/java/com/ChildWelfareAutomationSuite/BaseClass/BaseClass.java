089c89df8962b0747c5054a5ee193378f27b3f0b
venkat.mareedu <venkat.mareedu@cgi.com>
2017-02-15 13:46:38 -0600

    Update the Browser to Firefox from chrome

--- selenium/com.ChildWelfareAutomationSuite.POC/src/test/java/com/ChildWelfareAutomationSuite/BaseClass/BaseClass.java
+++ selenium/com.ChildWelfareAutomationSuite.POC/src/test/java/com/ChildWelfareAutomationSuite/BaseClass/BaseClass.java
@@ -22,7 +22,7 @@ public abstract class BaseClass {
 	@BeforeMethod
 	public void setUp() {
 		// Launch the specific Browser type
-		driver = BrowserFactory.getBrowser("chrome");
+		driver = BrowserFactory.getBrowser("firefox");
 		driver.get(DataProviderFactory.getConfig().getApplicationUrl());
 		BrowserFactory.impliciteWait(25);
 
