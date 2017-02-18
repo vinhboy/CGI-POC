bbaac97fcdf82ca37daaffa298cdab027f856cee
cjekal <charles.jekal@gmail.com>
2017-02-17 22:31:27 -0800

    forgot to commit

--- angular/app/scripts/controllers/profileCtrl.js
+++ angular/app/scripts/controllers/profileCtrl.js
@@ -12,26 +12,35 @@ cgiWebApp.controller('ProfileController',
   ['$scope', 'ProfileService', '$state',
   function ($scope, ProfileService, $state) {
 
+  $scope.restApiErrors = [];
+
   $scope.profile = {
     firstName: '',
     lastName: '',
     email: '',
     password: '',
     passwordConfirmation: '',
+    phoneNumber: {
+      areaCode: '',
+      centralOfficeCode: '',
+      lineNumber: ''
+    },
     phone: '',
     zipCode: '',
     emailNotification: false,
     pushNotification: false,
     smsNotification: false,
-    geoNotification: false,
     notificationType: [],
     latitude: 0,
-    longitude: 0
+    longitude: 0,
+    allowNotificationsByLocation: false
   };
 
   $scope.regexZip = /^\d{5}$/;
-  $scope.regexPhone = /^\d{3}-?\d{3}-?\d{4}$/;
   $scope.regexPassword = /^(?=.{8,})((?=.*\d)(?=.*[a-z])(?=.*[A-Z])|(?=.*\d)(?=.*[a-zA-Z])(?=.*[\W_])|(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_])).*/;
+  $scope.regexPhoneAreaCode = /^\d{3}$/;
+  $scope.regexPhoneCentralOfficeCode = /^\d{3}$/;
+  $scope.regexPhoneLineNumber = /^\d{4}$/;
 
   $scope.processNotificationTypes = function() {
     var notificationTypes = [];
@@ -47,8 +56,15 @@ cgiWebApp.controller('ProfileController',
     $scope.profile.notificationType = notificationTypes;
   };
 
+  $scope.generatePhoneNumber = function() {
+    $scope.profile.phone = $scope.profile.phoneNumber.areaCode +
+      $scope.profile.phoneNumber.centralOfficeCode +
+      $scope.profile.phoneNumber.lineNumber;
+  };
+
   $scope.registerProfile = function() {
     $scope.processNotificationTypes();
+    $scope.generatePhoneNumber();
 
     var toPost = {
       email: $scope.profile.email,
@@ -66,13 +82,13 @@ cgiWebApp.controller('ProfileController',
       if (response.status === 200) {
         $state.go('landing');
       }
-    }).catch(function(){
+    }).catch(function(response){
       window.alert('something went very wrong.');
     });
   };
 
   $scope.someSelected = function() {
-    return $scope.profile.emailNotification || $scope.profile.pushNotification || $scope.profile.smsNotification || $scope.profile.geoNotification;
+    return $scope.profile.emailNotification || $scope.profile.pushNotification || $scope.profile.smsNotification;
   };
 }]);
 
