2ef5d9e1f483320320c7433ba193d5694e2bd29d
cjekal <charles.jekal@gmail.com>
2017-02-23 04:20:46 -0800

    cleaning up PasswordlessUser

--- dropwizard/src/main/java/com/cgi/poc/dw/dao/model/PasswordlessUser.java
+++ dropwizard/src/main/java/com/cgi/poc/dw/dao/model/PasswordlessUser.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cgi.poc.dw.dao.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -13,16 +8,6 @@ import java.lang.reflect.InvocationTargetException;
 public class PasswordlessUser extends User {
 
   public PasswordlessUser() {
-  }
-
-  public PasswordlessUser(Long id) {
-    super(id);
-  }
-
-  public PasswordlessUser(Long id, String firstName, String lastName, String email,
-                          String phone, String zipCode, String role, double latitude, double longitude, String address1,
-                          String address2, String city, String state, Boolean emailNotification, Boolean pushNotification, Boolean smsNotification) {
-    super(id, firstName, lastName, email, null, phone, zipCode, role, latitude, longitude, address1, address2, city, state, emailNotification, pushNotification, smsNotification);
   }
 
   @Override
@@ -36,7 +21,8 @@ public class PasswordlessUser extends User {
   public void setPassword(String password) {
   }
 
-  public void copyFrom(User user) throws InvocationTargetException, IllegalAccessException {
+  public PasswordlessUser copyFrom(User user) throws InvocationTargetException, IllegalAccessException {
     BeanUtils.copyProperties(this, user);
+    return this;
   }
 }
